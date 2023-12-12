326e91e5a9716ae771fbc0ae0ffd3434b0d26ec2
USER <USER@DESKTOP-VLMS8KN>
2023-12-12 23:53:31 +0000

    Add project files.

---
+++ WebApplication6/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement2.Models
+{
+    public class Customer
+    {
+
+        [Key]
+        public int CustomerId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? LastName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string? Email { get; set; }
+    }
+}
---
+++ WebApplication6/Models/TaskManagerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserManagement2.Models
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions<TaskManagerContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; } = null!;
+    }
+}
